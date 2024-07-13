91e3439e84dc59562bf846bd70e6c483b35a25c8
Mr.Z <2838894779@qq.com>
2024-07-13 13:34:20 +0800

    修复了一个wildcard bug

--- wildcards.py
+++ wildcards.py
@@ -6,17 +6,23 @@ def dict_wildcard_match(prompt : str, prompt_dict : Dict[str, str]):
     pattern = re.compile(r'<<(.*?)>>')
     matches = re.findall(pattern, prompt)
     for match in matches:
-        text = prompt_dict[match]
-        prompt = prompt.replace(f'<<{match}>>', text)
+        try:
+            text = prompt_dict[match]
+            prompt = prompt.replace(f'<<{match}>>', text)
+        except:
+            pass
     return prompt
 def wildcard_match(prompt : str, dir_path : str):
     pattern = re.compile(r'<(.*?)>')
     matches = re.findall(pattern, prompt)
     for match in matches:
-        with open(f'{os.path.join(dir_path, match)}.txt', 'r') as f:
-            text = f.read().strip().split('\n')
-        text = random.choice(text)
-        prompt = prompt.replace(f'<{match}>', text)
+        try:
+            with open(f'{os.path.join(dir_path, match)}.txt', 'r') as f:
+                text = f.read().strip().split('\n')
+            text = random.choice(text)
+            prompt = prompt.replace(f'<{match}>', text)
+        except:
+            pass
     return prompt
 if __name__ == '__main__':
     p = wildcard_match("1girl, <general>, <artist>, somewords", 'suzuran', 'wildcards')
